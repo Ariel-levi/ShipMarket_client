6191616cdf2ef46f94dad0010bbd327295145d61
Nati <ronennt@gmail.com>
2022-06-07 15:21:38 +0300

    "search_address"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+<<<<<<< HEAD
 import "./App.css";
 import AppRoutes from "./appRoutes";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 358ad85 (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <AppRoutes />
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 358ad85 (Initialize project using Create React App)
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,9 +7,25 @@ import App from './App';
 
 ReactDOM.render(<App />,
   document.getElementById('root')
+=======
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+>>>>>>> 358ad85 (Initialize project using Create React App)
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< HEAD
 // reportWebVitals();
+=======
+reportWebVitals();
+>>>>>>> 358ad85 (Initialize project using Create React App)
