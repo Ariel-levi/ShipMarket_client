53d29b70fb206d98c38f74fc6a487af900825786
Idan Levi <iidanlevii@gmail.com>
2022-06-13 21:47:58 +0300

    delete for pr, not MERGE

--- src/misc_comps/dateCreated.js
+++
@@ -1,6 +0,0 @@
-export const dateCreated = (_date) => {
-  let d = new Date(_date);
-  const [month, day, year] = [d.getMonth() + 1, d.getDate(), d.getFullYear()];
-  let fullDate = year + "/" + month + "/" + day;
-  return fullDate;
-};
