fa70ef91ce649a97e304731f3874c4e66659dba9
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2022-06-29 14:25:35 +0300

    responsive nav + fix home strip text

--- src/comps_admin/headerAdmin.js
+++ src/comps_admin/headerAdmin.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import {
-  MdAdminPanelSettings,
-  MdOutlineLogout,
-  MdStorefront,
-} from "react-icons/md";
-import { BiHomeAlt, BiCategory } from "react-icons/bi";
-import { HiOutlineClipboardList } from "react-icons/hi";
-import { RiShoppingCartLine } from "react-icons/ri";
-import { FaUserAstronaut } from "react-icons/fa";
-import { checkTokenLocal } from "../services/localService";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { MdAdminPanelSettings, MdOutlineLogout, MdStorefront } from 'react-icons/md';
+import { BiHomeAlt, BiCategory } from 'react-icons/bi';
+import { HiOutlineClipboardList } from 'react-icons/hi';
+import { RiShoppingCartLine } from 'react-icons/ri';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { checkTokenLocal } from '../services/localService';
 
 function HeaderAdmin(props) {
-  let [login, setLogin] = useState("");
+  let [login, setLogin] = useState('');
   let location = useLocation();
 
   useEffect(() => {
@@ -31,16 +27,14 @@ function HeaderAdmin(props) {
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
+            aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
             className="offcanvas offcanvas-end"
             tabIndex="-1"
             id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
+            aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
                 Admin Panel <MdAdminPanelSettings className="ms-2" />
@@ -49,53 +43,49 @@ function HeaderAdmin(props) {
                 type="button"
                 className="btn-close"
                 data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
+                aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-start flex-grow-1 ps-5">
                 {!login ? (
-                  <li>
+                  <li data-bs-dismiss="offcanvas">
                     <Link to="/" className="nav-link">
                       Go Home <BiHomeAlt className="ms-1" />
                     </Link>
                   </li>
                 ) : (
                   <React.Fragment>
-                    <li>
-                      <Link
-                        to="/admin/logout"
-                        className="nav-link rounded text-danger"
-                      >
+                    <li data-bs-dismiss="offcanvas">
+                      <Link to="/admin/logout" className="nav-link rounded text-danger">
                         Log out <MdOutlineLogout className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/home">
                         Home <BiHomeAlt className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/orders">
                         Orders <HiOutlineClipboardList className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/users">
                         Users <FaUserAstronaut className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/stores">
                         Stores <MdStorefront className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/categories">
                         Categories <BiCategory className="ms-1" />
                       </Link>
                     </li>
-                    <li>
+                    <li data-bs-dismiss="offcanvas">
                       <Link className="nav-link" to="/admin/products">
                         Products <RiShoppingCartLine className="ms-1" />
                       </Link>
--- src/comps_storeAdmin/headerStore.js
+++ src/comps_storeAdmin/headerStore.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { BiHomeAlt } from "react-icons/bi";
-import { MdStorefront } from "react-icons/md";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiHomeAlt } from 'react-icons/bi';
+import { MdStorefront } from 'react-icons/md';
 
 function HeaderStore(props) {
   return (
@@ -16,16 +16,14 @@ function HeaderStore(props) {
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
+            aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
             className="offcanvas offcanvas-end"
             tabIndex="-1"
             id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
+            aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
                 AdminStore
@@ -34,17 +32,16 @@ function HeaderStore(props) {
                 type="button"
                 className="btn-close"
                 data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
+                aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-start flex-grow-1 ps-5">
-                <li>
+                <li data-bs-dismiss="offcanvas">
                   <Link className="nav-link" to="/storeAdmin">
                     My Stores <MdStorefront className="ms-1" />
                   </Link>
                 </li>
-                <li>
+                <li data-bs-dismiss="offcanvas">
                   <Link className="nav-link" to="/">
                     Go Home <BiHomeAlt className="ms-1" />
                   </Link>
